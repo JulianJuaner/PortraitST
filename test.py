c1d43563542ebb9bcf9d1eea6c2d43f13bcdffc2
JulianJuaner <37148692+JulianJuaner@users.noreply.github.com>
2019-12-27 16:45:27 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+import sys 
+sys.path.insert(1, './gainmap')
+sys.path.insert(1, './alignment')
+sys.path.insert(1, '../options')
+
+from dataset import *
+from modify import *
+from gainmap_option import *
